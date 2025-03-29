ab1fe63ef5d23ce25de8b07ee245a60a10a44f15
Your Name <you@example.com>
2025-03-29 17:01:03 +0000

    parametrized backend url

--- main_streamlit.py
+++ main_streamlit.py
@@ -1,6 +1,12 @@
+import os
+
 from hydra import compose, initialize
 
 from app.frontend.streamlit import streamlit_app
+
+BACKEND_URL = os.getenv(
+    "BACKEND_API_URL", "http://backend:8000/dates_coords_selection/"
+)
 
 if __name__ == "__main__":
     with initialize(version_base=None, config_path="./"):
@@ -8,5 +14,5 @@ if __name__ == "__main__":
 
     streamlit_app(
         config=config,
-        submit_url="http://backend:8000/dates_coords_selection/",
+        submit_url=BACKEND_URL,
     )
